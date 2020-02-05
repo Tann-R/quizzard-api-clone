87a89b10f81a565b3f6dfd1f8b7d939cbef960d7
Tannilen Reagan <tannilen.reagan@gmail.com>
2020-02-05 14:32:50 -0500

    Trying something else

--- src/main/java/com/revature/quizzard/servlets/TestServlet.java
+++ src/main/java/com/revature/quizzard/servlets/TestServlet.java
@@ -1,8 +1,16 @@
 package com.revature.quizzard.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @WebServlet ("/test")
 public class TestServlet extends HttpServlet {
-    System.out.println("This is a test.");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Application successfully deployed using AWS CodeBuild, CodePipeline, and Elastic Beanstalk");
+    }
 }
