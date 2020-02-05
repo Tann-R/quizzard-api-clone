517f9211f7d5dc2697d20b6f49d486447e7fc3eb
Tannilen Reagan <tannilen.reagan@gmail.com>
2020-02-05 14:30:26 -0500

    Typos are the bane of my existence.

--- src/main/java/com/revature/quizzard/servlets/TestServlet.java
+++ src/main/java/com/revature/quizzard/servlets/TestServlet.java
@@ -4,5 +4,5 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 @WebServlet ("/test")
 public class TestServlet extends HttpServlet {
-    System.out.println("This is a test.")
+    System.out.println("This is a test.");
 }
